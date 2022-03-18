e21a6fe3bc431ae143e6408ad2a61359015ef05f
FranUcles <ucles2014@hotmail.com>
2022-03-18 18:08:46 +0100

    Generador terminado, creada una clase para ello

--- Generador.cpp
+++ Generador.cpp
@@ -1,56 +1,121 @@
 #include <iostream>
-#include<time.h>
+#include <sys/time.h>
 using namespace std;
+
+
+class Generador // Objeto que genera los tiempos del algoritmo
+{
+private:
+    double tiempo_mejor;
+    double tiempo_peor;
+    double tiempo_promedio;
+
+public:
+    Generador();
+    void generarTiempos(int n, int m); // Calcula los tiempos en función de los parámetros
+    double getTiempoMejor(); // Obtener el tiempo mejor
+    double getTiempoPeor(); // Obtener el tiempo peor
+    double getTiempoPromedio(); // Obtener el tiempo promedio
+};
+
+Generador::Generador()
+{
+    tiempo_mejor = 0;
+    tiempo_peor = 0;
+    tiempo_promedio = 0;
+}
 
 const int NUMERO_PRUEBAS_PROMEDIO = 10;
 
-string generar_string_promedio(int n) {
+string generar_string_promedio(int n)
+{
     string resultado = string();
-    srand(time(NULL));  // Da un generador de valores aleatorios con semilla en función de la hora actual
-    for (int i = 0; i < n; i++) {
+    srand(time(NULL)); // Da un generador de valores aleatorios con semilla en función de la hora actual
+    for (int i = 0; i < n; i++)
+    {
         char valor = 'a' + (rand() % ('z' - 'a' + 1)); // Genera un random, lo acota por el número de letras y se lo añade a la 'a'
-        resultado += valor; // Concatena los resultados
+        resultado += valor;                            // Concatena los resultados
     }
     return resultado;
 }
 
-string generar_string_igual(int n, char c) {
+string generar_string_igual(int n, char c)
+{
     string resultado = string();
     for (int i = 0; i < n; i++)
-        resultado += c; 
+        resultado += c;
     return resultado;
 }
 
-double media(double casos[]){
+double media(double casos[])
+{
     double suma = 0;
-    int tamano = sizeof(casos)/sizeof(casos[0]); // Tamaño del array de casos
+    int tamano = sizeof(casos) / sizeof(casos[0]); // Tamaño del array de casos
     for (int i = 0; i < tamano; i++)
         suma += casos[i];
-    return suma/tamano;
+    return suma / tamano;
 }
 
-double caso_promedio(int n, int m) {
-    double casos[NUMERO_PRUEBAS_PROMEDIO];  // Array donde guardar las diferentes salidas de los casos
-    for (int i = 0; i < NUMERO_PRUEBAS_PROMEDIO; i++) {
+double caso_promedio(int n, int m)
+{
+    double casos[NUMERO_PRUEBAS_PROMEDIO]; // Array donde guardar las diferentes salidas de los casos
+    struct timeval ti,tf; // Hora de inicio y hora de fin
+    double tiempo; // Tiempo transcurrido
+    for (int i = 0; i < NUMERO_PRUEBAS_PROMEDIO; i++)
+    {
         string cadena_A = generar_string_promedio(n);
         string cadena_B = generar_string_promedio(n);
-        // casos[i] = DyV(m, cadena_A, cadena_B).time();
+        gettimeofday(&ti,NULL); // Toma la hora antes del algoritmo
+        // DyV(m, cadena_A, cadena_B);
+        gettimeofday(&tf,NULL); // Toma la hora después del algoritmo
+        tiempo = (tf.tv_sec - ti.tv_sec)*1000 + (tf.tv_usec - ti.tv_usec)/1000.0; // Calcula el tiempo transcurrido
+        casos[i] = tiempo;
     }
     return media(casos);
 }
 
-double caso_mejor(int n, int m) {
+double caso_mejor(int n, int m)
+{
     string cadena_A = generar_string_igual(n, 'a');
     string cadena_B = generar_string_igual(n, 'a');
-    // return DyV(m, cadena_A, cadena_B).time();
+    struct timeval ti,tf; // Hora de inicio y hora de fin
+    double tiempo; // Tiempo transcurrido
+    gettimeofday(&ti,NULL); // Toma la hora antes del algoritmo
+    // DyV(m, cadena_A, cadena_B);
+    gettimeofday(&tf,NULL); // Toma la hora después del algoritmo
+    tiempo = (tf.tv_sec - ti.tv_sec)*1000 + (tf.tv_usec - ti.tv_usec)/1000.0; // Calcula el tiempo transcurrido
+    return tiempo;
 }
 
-double caso_peor(int n, int m) {
+double caso_peor(int n, int m)
+{
     string cadena_A = generar_string_igual(n, 'a');
     string cadena_B = generar_string_igual(n, 'b');
-    // return DyV(m, cadena_A, cadena_B).time();
+    struct timeval ti,tf; // Hora de inicio y hora de fin
+    double tiempo; // Tiempo transcurrido
+    gettimeofday(&ti,NULL); // Toma la hora antes del algoritmo
+    // DyV(m, cadena_A, cadena_B);
+    gettimeofday(&tf,NULL); // Toma la hora después del algoritmo
+    tiempo = (tf.tv_sec - ti.tv_sec)*1000 + (tf.tv_usec - ti.tv_usec)/1000.0; // Calcula el tiempo transcurrido
+    return tiempo;
 }
 
-int main(){
+void Generador::generarTiempos(int n, int m)
+{
+    this->tiempo_mejor = caso_mejor(n,m);
+    this->tiempo_peor = caso_peor(n,m);
+    this->tiempo_promedio = caso_promedio(n,m);
+}
 
+double Generador::getTiempoPeor()
+{
+    return this->tiempo_peor;
+}
+double Generador::getTiempoMejor()
+{
+    return this->tiempo_mejor;
+}
+double Generador::getTiempoPromedio()
+{
+    return this->tiempo_promedio;
 }

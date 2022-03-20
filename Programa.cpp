067a82a2a5b8faa14f5eb32f438d972bd3a47265
FranUcles <ucles2014@hotmail.com>
2022-03-20 13:05:13 +0100

    Prueba

--- Programa.cpp
+++ Programa.cpp
@@ -29,5 +29,25 @@ Dato combinar(int m, int inicio, int final, int A[], int B[], Dato parcial1, Dat
     }
     return Dato::datoMax(parcial3, Dato::datoMax(parcial1, parcial2)); // Devolvemos el máximo de las tres soluciones parciales
 }
+Dato resolucionDirecta(char A[], char B[], int inicio, int final){
+    Dato resultado(inicio, 0);
+    int max = 0;
+    int cont = 0;
 
-int main(int argc, char const *argv[]);
+    for(int i = inicio; i<=final; i++){
+        if (A[i] == B[i])   cont++;
+        else{
+            if (cont >= max)    max = cont;
+            cont = 0;
+        }
+    }
+
+    resultado.setLong(max);
+
+    return resultado;
+
+}
+
+int main(int argc, char const *argv[])
+{
+}

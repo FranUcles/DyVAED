6b680efc690ba1e4d1053b3b225014a503dd0e65
FranUcles <ucles2014@hotmail.com>
2022-03-20 13:13:24 +0100

    Función: Combinar Fixes #3

--- Programa.cpp
+++ Programa.cpp
@@ -15,9 +15,19 @@ Dato combinar(int m, int inicio, int final, int A[], int B[], Dato parcial1, Dat
     if (matches == m)
         return parcial3;
     int longitud = min(final - inicio + 1, 2*m - 1);
-    for (int i = 1; i <= 2*m - 1; i++){ // No es hasta 2*m - 1
-
+    for (int i = 1; i <= longitud; i++){ // No es hasta 2*m - 1
+        if (A[inicio + 1] == B[inicio + 1] && A[inicio + m + i] != B[inicio + m + i])
+            matches--;
+        else if (A[inicio + 1] != B[inicio + 1] && A[inicio + m + i] == B[inicio + m + i])
+            matches++;
+        if (matches > parcial3.getLong()) {
+            parcial3.setPos(inicio + i);
+            parcial3.setLong(matches);
+        }
+        if (parcial3.getLong() == m)
+            return parcial3;
     }
+    return Dato::datoMax(parcial3, Dato::datoMax(parcial1, parcial2));
 }
 
 int main(int argc, char const *argv[])

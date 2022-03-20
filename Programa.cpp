574b4e5fb734e604e53d40e276a356d660f47866
FranUcles <ucles2014@hotmail.com>
2022-03-20 17:25:23 +0100

    ResolucionDirecta

--- Programa.cpp
+++ Programa.cpp
@@ -33,11 +33,12 @@ Dato resolucionDirecta(char A[], char B[], int inicio, int final){
     Dato resultado(inicio, 0);
     int max = 0;
     int cont = 0;
-
-    for(int i = inicio; i<=final; i++){
-        if (A[i] == B[i])   cont++;
+    for (int i = inicio; i <= final; i++){
+        if (A[i] == B[i])
+            cont++;
         else{
-            if (cont >= max)    max = cont;
+            if (cont >= max)
+                max = cont;
             cont = 0;
         }
     }
@@ -45,7 +46,6 @@ Dato resolucionDirecta(char A[], char B[], int inicio, int final){
     resultado.setLong(max);
 
     return resultado;
-
 }
 
 int main(int argc, char const *argv[])
